d6abec44a5f1dfd0b4e7828e72c25f0bb4ea1f8b
Montana <montanaavic10@gmai.com>
2026-03-05 13:02:55 -0600

    ultimo

--- app.js
+++ app.js
@@ -19,3 +19,21 @@ window.location.href = "login-cliente.html";
 function negocioLogin(){
 window.location.href = "login-negocio.html";
 }
+
+function irRegistro(){
+
+window.location.href = "registro.html";
+
+}
+
+function clienteRegistro(){
+
+window.location.href = "registro-cliente.html";
+
+}
+
+function negocioRegistro(){
+
+window.location.href = "registro-negocio.html";
+
+}
